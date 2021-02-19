8fdfe632280df0dc8f588d69ef126f362c9d413f
thanhdoan <thanh.doan1@rbi.co.uk>
2021-02-19 15:25:39 +0000

    Styling componen

--- ng-fundamentals/src/app/events/event-thumbail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbail.component.ts
@@ -10,14 +10,25 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       <div>Price: £{{ event.price }}</div>
       <div>
         <span>Location: {{ event.location.address }}</span>
-        <span>&nbsp;</span>
-        <span>{{ event.location.city }}, {{ event.location.country }}</span>
+        <span class="pad-left">
+          {{ event.location.city }}, {{ event.location.country }}
+        </span>
       </div>
       <button class="btn btn-primary" (click)="handleClickMe()">
         Click me
       </button>
     </div>
   `,
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: red;
+      }
+    `,
+  ],
 })
 export class EventThumbnailComponent {
   @Input() event: any;
