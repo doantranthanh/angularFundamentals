eab3dba6915e158722f486e32c2dd82d3a86c264
thanhdoan <thanh.doan1@rbi.co.uk>
2021-03-02 14:03:39 +0000

    Link to Routes

--- ng-fundamentals/src/app/events/event-thumbail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbail.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector: "event-thumbnail",
+    selector: 'event-thumbnail',
     template: `
-        <div class="well hoverwell thumbnail">
+        <div
+            [routerLink]="['/events', event.id]"
+            class="well hoverwell thumbnail"
+        >
             <h2>{{ event?.name }}</h2>
             <div>Date: {{ event?.date }}</div>
             <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
@@ -61,8 +64,8 @@ export class EventThumbnailComponent {
     //     console.log("Foo");
     // }
     getStartTimeClass() {
-        if (this.event && this.event.time === "8:00 am") {
-            return ["green", "bold"];
+        if (this.event && this.event.time === '8:00 am') {
+            return ['green', 'bold'];
         }
         return [];
     }
