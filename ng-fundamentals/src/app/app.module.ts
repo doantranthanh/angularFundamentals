c06793d0a950d51e20d442af947f2746020f4ba5
thanhdoan <thanh.doan1@rbi.co.uk>
2021-03-02 14:12:18 +0000

    Add create event component

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
 
 @NgModule({
     declarations: [
@@ -18,6 +19,7 @@ import { appRoutes } from './routes';
         EventThumbnailComponent,
         NavBarComponent,
         EventDetailsComponent,
+        CreateEventComponent,
     ],
     imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
     providers: [EventService, ToastrService],
