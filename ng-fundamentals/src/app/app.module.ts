420c5678d153f4e97ca8d02ced8cde5072596a34
thanhdoan <thanh.doan1@rbi.co.uk>
2021-03-02 13:54:42 +0000

    # (Optional) Provide links or keys to any relevant tickets, articles or other resources # Example: Github issue #23 ADD ROUTES # --- COMMIT END --- # Tag can be #    feat     (new feature) #    fix      (bug fix) #    refactor (refactoring code) #    style    (formatting, missing semi colons, etc; no code change) #    doc      (changes to documentation) #    test     (adding or refactoring tests; no production code change) #    version  (version bump/new release; no production code change) #    jsrXXX   (Patches related to the implementation of jsrXXX, where XXX the JSR number) #    jdkX     (Patches related to supporting jdkX as the host VM, where X the JDK version) #    dbg      (Changes in debugging code/frameworks; no production code change) #    license  (Edits regarding licensing; no production code change) #    hack     (Temporary fix to make things move forward; please avoid it) #    WIP      (Work In Progress; for intermediate commits to keep patches reasonably sized) #    defaults (changes default options) # # Note: Multiple tags can be combined, e.g. [fix][jsr292] Fix issue X with methodhandles # -------------------- # Remember to: #   * Capitalize the subject line #   * Use the imperative mood in the subject line #   * Do not end the subject line with a period #   * Separate subject from body with a blank line #   * Use the body to explain what and why vs. how #   * Can use multiple lines with "-" or "*" for bullet points in body # --------------------

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -7,6 +8,8 @@ import { EventThumbnailComponent } from './events/event-thumbail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
     declarations: [
@@ -14,8 +17,9 @@ import { ToastrService } from './common/toastr.service';
         EventsListComponent,
         EventThumbnailComponent,
         NavBarComponent,
+        EventDetailsComponent,
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
     providers: [EventService, ToastrService],
     bootstrap: [EventsAppComponent],
 })
